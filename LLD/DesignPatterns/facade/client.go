cc71db18e73807e458a2b268cddd97f95c5b483c
dhakad22klx <dhakad22klx.pvt@gmail.com>
2026-05-01 17:47:46 +0530

    added Facade method

---
+++ LLD/DesignPatterns/facade/client.go
@@ -0,0 +1,29 @@
+package facade
+
+import (
+	"fmt"
+	"log"
+)
+
+/*
+Facade is a structural design pattern
+that provides a simplified (but limited) interface
+to a complex system of classes, library or framework.
+*/
+
+func TestFacadePattern() {
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
