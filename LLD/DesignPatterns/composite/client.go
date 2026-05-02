a5b240fa7d0647eb628dc6e2aa4b55003be3c250
dhakad22klx <dhakad22klx.pvt@gmail.com>
2026-05-02 14:40:17 +0530

    Added composite pattern

--- LLD/DesignPatterns/composite/client.go
+++ LLD/DesignPatterns/composite/client.go
@@ -1,8 +1,24 @@
 package composite
 
 /*
+The Composite Design Pattern is a structural pattern that lets you treat individual objects
+and compositions of objects uniformly.
 
- */
+It allows you to build tree-like structures (e.g., file systems, UI hierarchies, organizational charts)
+where clients can work with both single elements and groups of elements using the same interface.
+
+It’s particularly useful in situations where:
+- You need to represent part-whole hierarchies.
+- You want to perform operations on both leaf nodes and composite nodes in a consistent way.
+- You want to avoid writing special-case logic to distinguish between "single" and "grouped" objects.
+
+Composite is a structural design pattern that lets you compose objects into tree structures
+and then work with these structures as if they were individual objects.
+
+Composite became a pretty popular solution for the most problems that require building a tree structure.
+Composite’s great feature is the ability to run methods recursively
+over the whole tree structure and sum up the results.
+*/
 
 import "fmt"
 
