c49b219acd2e3007a7c2e7e2308003dc205419b6
dhakad22klx <dhakad22klx.pvt@gmail.com>
2026-05-24 16:21:45 +0530

    Modified Content of  Mediator Pattern

--- LLD/DesignPatterns/mediator/client.go
+++ LLD/DesignPatterns/mediator/client.go
@@ -1,6 +1,15 @@
 package mediator
 
 /*
+The Mediator Design Pattern is a behavioral pattern that defines
+an object (the Mediator) to encapsulate how a set of objects interact.
+
+Mediator is a behavioral design pattern that reduces coupling between components of a program
+by making them communicate indirectly, through a special mediator object.
+
+The Mediator makes it easy to modify, extend and reuse individual components because
+they’re no longer dependent on the dozens of other classes.
+
 Observer Vs Mediator
 Observer — One source, many listeners.
 Mediator — Many components, one coordinator.
