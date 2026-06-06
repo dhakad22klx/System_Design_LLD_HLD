04c20bbca3f38a8cc7d9eca6745c10a051573738
dhakad22klx <dhakad22klx.pvt@gmail.com>
2026-06-06 10:54:07 +0530

    Implemented lru cache

--- Problems/LRU-Cache/node.go
+++ Problems/LRU-Cache/node.go
@@ -1,5 +1,7 @@
 package main
 
+// [K comparable, V any] : The Type Parameter List. It says
+// "This struct uses two placeholders, K and V, which can be comparable and any type."
 type Node[K comparable, V any] struct {
 	key   K
 	value V
