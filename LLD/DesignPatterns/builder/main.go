7b12f7ef46b0317d52c6941f945d88ad84787590
dhakad22klx <dhakad22klx.pvt@gmail.com>
2026-06-06 17:39:54 +0530

    Theory of builder pattern upgraded

--- LLD/DesignPatterns/builder/main.go
+++ LLD/DesignPatterns/builder/main.go
@@ -1,5 +1,20 @@
 package builder
 
+/*The Builder Design Pattern is a creational pattern that lets you construct complex objects step-by-step,
+separating the construction logic from the final representation.
+
+It’s particularly useful in situations where:
+
+- An object has many optional fields, and most callers only need a subset.
+- You want to avoid telescoping constructors or long parameter lists.
+- The object must be assembled through multiple steps, possibly in a specific order.
+----
+
+Builder is a creational design pattern, which allows constructing complex objects step by step.
+
+Unlike other creational patterns, Builder doesn’t require products to have a common interface.
+That makes it possible to produce different products using the same construction process.
+*/
 import (
 	"fmt"
 )
